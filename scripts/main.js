72fca610ce4717c2691629c5f915bcb7886ec1eb
Daniel Romero <romerodaniel.k90@gmail.com>
2017-04-22 13:23:30 -0700

    tweaking color transitions

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
 var genesis = (function() {
   var clickCounter = 0;
   var body = document.getElementsByTagName('body')[0];
+  var socialMediaIcons = document.getElementsByClassName('main-container-social-links-list-item');
+
   var colorRandomizer = function() {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     body.style.background = `#${randomColor}`;
-    body.style.transition = '4s';
+    body.style.transition = '3.5s';
+
+    for (var index = 0; index < socialMediaIcons.length; index++) {
+      socialMediaIcons[index].style.color = `#${randomColor}`
+      socialMediaIcons[index].style.transition = '2s';
+    }
   };
 
-  var secondIntervals = setInterval(colorRandomizer, 1000);
+  var secondIntervals = setInterval(colorRandomizer, 1250);
 
   body.onclick = function(event) {
     clickCounter = clickCounter + 1;
