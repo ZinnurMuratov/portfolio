1615470e33babcee97ea4e200384a1ace2900f2f
Daniel Romero <romerodaniel.k90@gmail.com>
2017-08-18 21:06:56 -0700

    introduce container class, needs: main component .container fix

--- client/components/core/footer/footer.component.ts
+++ client/components/core/footer/footer.component.ts
@@ -4,18 +4,20 @@ import Component from 'vue-class-component';
 @Component({
   template: `
     <footer class="footer-component">
-      <section class="footer-section">
-        <ul class="flat-list center">
-          <li v-for="link in socialLinks">
-            <a v-bind:href="link.url" class="social-icon">
-              <img
-                :srcset="link.icon.src.retina + ' 1.25x'"
-                :src="link.icon.src.nonRetina"
-                :alt="link.icon.alt"/>
-            </a>
-          </li>
-        </ul>
-      </section>
+      <div class="container">
+        <section class="footer-section">
+          <ul class="flat-list center">
+            <li v-for="link in socialLinks">
+              <a v-bind:href="link.url" class="social-icon">
+                <img
+                  :srcset="link.icon.src.retina + ' 1.25x'"
+                  :src="link.icon.src.nonRetina"
+                  :alt="link.icon.alt"/>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
     </footer>
   `,
 })
--- client/components/core/nav/nav.component.ts
+++ client/components/core/nav/nav.component.ts
@@ -4,16 +4,18 @@ import Component from 'vue-class-component';
 @Component({
   template: `
     <nav class="navigation-component">
-      <section class="nav-section">
-        <ul class="flat-list right navigation-links">
-          <li class="nav-link">
-            <router-link :to="{ path: '/' }">Home</router-link>
-          </li>
-          <li class="nav-link">
-            <router-link :to="{ path: '/works' }">Works</router-link>
-          </li>
-        </ul>
-      </section>
+      <div class="container">
+        <section class="nav-section">
+          <ul class="flat-list right navigation-links">
+            <li class="nav-link">
+              <router-link :to="{ path: '/' }">Home</router-link>
+            </li>
+            <li class="nav-link">
+              <router-link :to="{ path: '/works' }">Works</router-link>
+            </li>
+          </ul>
+        </section>
+      </div>
     </nav>
   `,
 })
