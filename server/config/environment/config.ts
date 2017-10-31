9f29ddce622f3b5d115ea4fb2da733d88b7d73f0
Daniel Romero <romerodaniel.k90@gmail.com>
2017-10-31 11:15:37 -0700

    finishing touches, needs: icon lib && flicker location api integration

--- server/config/environment/config.ts
+++ server/config/environment/config.ts
@@ -1,4 +1,5 @@
 import { join, resolve } from 'path';
+import { keys } from './secret';
 
 export const config = {
   env: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
@@ -6,4 +7,7 @@ export const config = {
   views_dir: resolve(__dirname, '..', '..', 'views'),
   client_assets_path: resolve(__dirname, '..', '..', '..', 'client'),
   port: process.env.PORT ? process.env.PORT : 3000,
+  keys: {
+    weather: keys.weather_api,
+  },
 };
--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -2,15 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import { lookup as geoLookup } from 'geoip-lite';
 import { address as selfIP } from 'ip';
 import * as requestLib from 'request';
+import { config } from './../config/environment/config';
 
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY || config.keys.weather;
 
 export function GetGeolocation(req: Request, res: Response, next: NextFunction) {
   const clientIP: string = req.headers['x-forwarded-for'] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
-  const IP_ADDRESS = process.env.NODE_ENV === 'production' ? clientIP : selfIP();
+  const IP_ADDRESS: string = process.env.NODE_ENV === 'production' ? clientIP : selfIP();
   return res.json(geoLookup(IP_ADDRESS));
 }
 
