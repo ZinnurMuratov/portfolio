b647d1611d82eb425680e37a2b4feb5469e43ec5
Daniel Romero <romerodaniel.k90@gmail.com>
2017-11-02 10:25:05 -0700

    clean up

--- server/controllers/quote.controller.ts
+++ server/controllers/quote.controller.ts
@@ -4,11 +4,8 @@ import * as requestLib from 'request';
 import { config } from './../config/environment/config';
 
 export function GetRandomQuote(req: Request, res: Response, next: NextFunction) {
-  if (!req.query) { return res.status(400).json({ error: 'Must include ...' }); }
-
   requestLib(config.urls.talaikis, { json: true }, (err: any, response: requestLib.RequestResponse, body: any) => {
     if (err) { return res.send({ data: err, success: false }); }
-
     return res.status(response.statusCode).json({
       data: body,
       success: response.statusCode >= 200 && response.statusCode <= 300,
