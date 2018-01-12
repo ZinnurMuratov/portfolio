3b6dbc340a1f246132240f2ece01f0b44124a7eb
Daniel Romero <romerodaniel.k90@gmail.com>
2018-01-12 15:36:24 -0800

    progress bar shows percentage, needs: links

--- client/components/core/index.ts
+++ client/components/core/index.ts
@@ -1,3 +1,4 @@
 export { NavComponent } from './nav/nav.component';
 export { HomeComponent } from './home/home.component';
 export { FooterComponent } from './footer/footer.component';
+export { MaintenanceComponent } from './maintenance/maintenance.component';
