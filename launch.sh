e813615e834aa806dc983c59be5fde386d2403eb
Daniel Romero <romerodaniel.k90@gmail.com>
2018-02-01 18:02:55 -0800

    set executable to prod, test worked

--- launch.sh
+++ launch.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# git fetch --all && git checkout --force "origin/master" &&
-# rm -rf node_modules && yarn &&  npm run build 
-
-mkdir -p .logs/stamps &&
-touch .logs/stamps/prod.js &&
 echo "PROD TIME: $(date)" >> .logs/stamps/prod.js
+git fetch --all && git checkout --force "origin/master" &&
+rm -rf node_modules && yarn &&  npm run build
