ee8297f632072dd9f60f0ef43f6c4e59e48bb510
Daniel Romero <romerodaniel.k90@gmail.com>
2018-02-20 22:51:33 -0800

    update launch scritps

--- launch.sh
+++ launch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-echo "PROD TIME: $(date)" >> .logs/stamps/prod.js
-git fetch --all && git checkout --force "origin/master" &&
-rm -rf node_modules && yarn &&  npm run build
+mkdir -p .logs/stamps &&
+echo "'PROD TIME: $(date)'" >> .logs/stamps/prod.js && 
+git checkout . &&
+rm -rf node_modules &&
+yarn install --production=false &&
+yarn build
